a8cfbe13358b206677e3e785d9c78fbbd504c21f
dsjramesh7 <rameshdussa07@gmail.com>
2026-06-06 23:32:36 +0530

    practice

---
+++ P13.py
@@ -0,0 +1,13 @@
+def majority_element(nums):
+    candidate = None
+    count = 0
+    
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += 1 if num == candidate else -1
+        
+    return candidate
+
+
+print(majority_element([2, 2, 1, 1, 1, 2, 2])) 
